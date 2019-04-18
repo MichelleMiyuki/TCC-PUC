88d97077abac53e1054829069b174685c4639ad0
MichelleMiyuki <43833195+MichelleMiyuki@users.noreply.github.com>
2019-04-17 21:26:20 -0300

    Alterando endereço do banco

--- config/conexao.php
+++ config/conexao.php
@@ -1,11 +1,20 @@
 <!-- [BACK-END] Arquivo para fazer a conexão do app com o banco de dados -->
 
 <?php
-define('HOST', 'localhost');
-define('USUARIO', 'root');
-define('SENHA', '');
-define('DB', 'alunos');
+$LOCAL = false;
 
-$conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Não foi possível conectar');
+$HOST = 'us-cdbr-iron-east-02.cleardb.net';
+$USUARIO = 'b546a5801bd90c';
+$SENHA = 'f7bf2d47';
+$DB = 'heroku_aa83e7a11e448e2';
+
+if ($LOCAL){
+    $HOST = 'localhost';
+    $USUARIO = 'root';
+    $SENHA = '';
+    $DB = 'alunos';
+}
+    
+$conexao = mysqli_connect($HOST, $USUARIO, $SENHA, $DB) or die ('Não foi possível conectar');
 
 ?>
